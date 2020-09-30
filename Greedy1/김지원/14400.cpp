cf81454fdb3a4b7e571b2e205926163c4ed4bcc5
GrayPenguin <GrayPenguin@users.noreply.github.com>
2020-09-30 15:59:36 +0900

    Update 14400.cpp

--- Greedy1/김지원/14400.cpp
+++ Greedy1/김지원/14400.cpp
@@ -29,7 +29,7 @@ int main(){
 	int conypos = posy[(n-1)/2];
 	
 	
-	int distance = 0;
+	long long int distance = 0;
 	
 	for(i=0;i<n;i++){
 		if((conxpos - posx[i]) >= 0) distance += conxpos - posx[i];
